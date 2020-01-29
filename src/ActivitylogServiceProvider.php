0aad7dd6536752425cea22df0b38f3774c4f4b0b
darkraul79 <info@raulsebastian.es>
2020-01-29 17:37:19 +0100

    service provider

--- src/ActivitylogServiceProvider.php
+++ src/ActivitylogServiceProvider.php
@@ -1,61 +1,61 @@
 <?php
 
-namespace Spatie\Activitylog;
+namespace darkraul79\Activitylog;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
-use Spatie\Activitylog\Models\Activity;
-use Spatie\Activitylog\Exceptions\InvalidConfiguration;
+use darkraul79\Activitylog\Models\Activity;
+use darkraul79\Activitylog\Exceptions\InvalidConfiguration;
 
 class ActivitylogServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap the application events.
-     */
-    public function boot()
-    {
-        $this->publishes([
-            __DIR__.'/../config/activitylog.php' => config_path('activitylog.php'),
-        ], 'config');
+	/**
+	 * Bootstrap the application events.
+	 */
+	public function boot()
+	{
+		$this->publishes([
+			__DIR__ . '/../config/activitylog.php' => config_path('activitylog.php'),
+		], 'config');
 
-        $this->mergeConfigFrom(__DIR__.'/../config/activitylog.php', 'activitylog');
+		$this->mergeConfigFrom(__DIR__ . '/../config/activitylog.php', 'activitylog');
 
-        if (! class_exists('CreateActivityLogTable')) {
-            $timestamp = date('Y_m_d_His', time());
+		if (!class_exists('CreateActivityLogTable')) {
+			$timestamp = date('Y_m_d_His', time());
 
-            $this->publishes([
-                __DIR__.'/../migrations/create_activity_log_table.php.stub' => database_path("/migrations/{$timestamp}_create_activity_log_table.php"),
-            ], 'migrations');
-        }
-    }
+			$this->publishes([
+				__DIR__ . '/../migrations/create_activity_log_table.php.stub' => database_path("/migrations/{$timestamp}_create_activity_log_table.php"),
+			], 'migrations');
+		}
+	}
 
-    /**
-     * Register the service provider.
-     */
-    public function register()
-    {
-        $this->app->bind('command.activitylog:clean', CleanActivitylogCommand::class);
+	/**
+	 * Register the service provider.
+	 */
+	public function register()
+	{
+		$this->app->bind('command.activitylog:clean', CleanActivitylogCommand::class);
 
-        $this->commands([
-            'command.activitylog:clean',
-        ]);
-    }
+		$this->commands([
+			'command.activitylog:clean',
+		]);
+	}
 
-    public static function determineActivityModel(): string
-    {
-        $activityModel = config('activitylog.activity_model') ?? Activity::class;
+	public static function determineActivityModel(): string
+	{
+		$activityModel = config('activitylog.activity_model') ?? Activity::class;
 
-        if (! is_a($activityModel, Activity::class, true)) {
-            throw InvalidConfiguration::modelIsNotValid($activityModel);
-        }
+		if (!is_a($activityModel, Activity::class, true)) {
+			throw InvalidConfiguration::modelIsNotValid($activityModel);
+		}
 
-        return $activityModel;
-    }
+		return $activityModel;
+	}
 
-    public static function getActivityModelInstance(): Model
-    {
-        $activityModelClassName = self::determineActivityModel();
+	public static function getActivityModelInstance(): Model
+	{
+		$activityModelClassName = self::determineActivityModel();
 
-        return new $activityModelClassName();
-    }
+		return new $activityModelClassName();
+	}
 }
